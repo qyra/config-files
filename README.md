e8aee497a81fc04a59699c567aadb9e0081985eb
CICD Autobump <cicd-autobump@entrupy.com>
2025-01-29 19:04:53 -0500

    adding mac keybindings + settings

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-Vscode Linux:
+# Vscode
+## Vscode Linux:
 ln -s $HOME/projects/config-files/vscode/keybindings.json $HOME/.config/Code/User
 ln -s $HOME/projects/config-files/vscode/settings.json $HOME/.config/Code/User
 ln -s $HOME/projects/config-files/vscode/snippets $HOME/.config/Code/User/snippets
 
+## Vscode Mac:
+ln -s "/Users/karl/projects/config-files/mac/Library/Application Support/Code/User/settings.json" "/Users/karl/Library/Application Support/Code/User/settings.json"
--- bin/startup.sh
+++ bin/startup.sh
@@ -10,11 +10,13 @@ cd $ENTRUPY_PROJECTS/local-development-scripts/dynamodb
 poetry env use python3.11
 
 # Required for public API tests.
-DUMP_PATH="$ENTRUPY_PROJECTS/app-api-public/tests/data/test-dumps/public_api-test-dump__2024-04-23__karl.dump/dev"
-time $ENTRUPY_PROJECTS/local-development-scripts/dynamodb/import_dump_poetry.sh -i $DUMP_PATH -d app --quiet --targets --flush
+time $ENTRUPY_PROJECTS/app-api-public/tests/test_data/import_test_data.sh
 
-DUMP_PATH="$ENTRUPY_PROJECTS/app-api-public/tests/data/test-dumps/public_api-test-dump__2024-04-23__karl.dump/prod"
-time $ENTRUPY_PROJECTS/local-development-scripts/dynamodb/import_dump_poetry.sh -i $DUMP_PATH -d app --quiet --targets
+# DUMP_PATH="$ENTRUPY_PROJECTS/app-api-public/tests/data/test-dumps/public_api-test-dump__2024-04-23__karl.dump/dev"
+# time $ENTRUPY_PROJECTS/local-development-scripts/dynamodb/import_dump_poetry.sh -i $DUMP_PATH -d app --quiet --targets --flush
+
+# DUMP_PATH="$ENTRUPY_PROJECTS/app-api-public/tests/data/test-dumps/public_api-test-dump__2024-04-23__karl.dump/prod"
+# time $ENTRUPY_PROJECTS/local-development-scripts/dynamodb/import_dump_poetry.sh -i $DUMP_PATH -d app --quiet --targets
 
 # # Required for app-model-service real item tests?
 # DUMP_PATH="/Users/karl/data/v2-dumps/2023-10-13-ams-dev-test-items"
